db99a7a20f4d4807724bff39abea4b79a74354bd
Adam Maślanka <adam.maslanka@polcode.net>
2017-01-20 18:30:44 +0100

    added images & style to refer page

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,14 +1,29 @@
-$(document).ready(function(){
-	var myVideo = document.getElementById('video');
-	console.log(myVideo);
-	if(myVideo){
-		if (typeof myVideo.loop == 'boolean') { // loop supported
-		    myVideo.loop = true;
-		} else { // loop property not supported
-		    myVideo.on('ended', function () {
-		    this.currentTime = 0;
-		    this.play();
-		    }, false);
-		}
-	}
+$(document).ready(function() {
+    var points = 49; // $('.rewards').data('points');
+    var progress_stop = false;
+    var rewards = [];
+
+    $('.reward').each(function(e) {
+        rewards.push($(this)); // put all rewards to array;
+    })
+
+    for (var i = rewards.length; i > 0; i--) {
+        if (!progress_stop) {
+
+            var _this = rewards[i - 1];
+						var min_points = $(_this).data('min');
+            var reward_points = parseInt($(_this).children('.points').text());
+            var r_points = 1.0 * reward_points;
+            var height_percent = (points / reward_points) * 100.0;
+            console.log(height_percent);
+
+            if (height_percent < 100)
+                var progress_stop = true;
+						if (height_percent >= 100)
+							_this.addClass('completed');
+						else if (progress_stop && min_points != points) {
+							$(_this).css('background', '-webkit-linear-gradient(bottom, #0068FF, #0068FF '+height_percent+'%, transparent '+height_percent+'%, transparent 100%)');
+						}
+        }
+    }
 })
