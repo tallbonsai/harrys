158319f71c7dc85e85d708a711ee238ddad9633b
Adam Maślanka <adam.maslanka@polcode.net>
2017-01-25 22:00:42 +0100

    Changed twitter message

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ActionMailer::Base
-  default from: "Harry's <welcome@referral.bonsai.com>"
+  default from: 'hello@shopbonsai.ca'
 
   def signup_email(user)
     @user = user
-    @twitter_message = "#Shaving is evolving. Excited for @harrys to launch."
+    @twitter_message = '#Shaving is evolving. Excited for @harrys to launch.'
 
-    mail(:to => user.email, :subject => "Thanks for signing up!")
+    mail(to: user.email, subject: 'Thanks for signing up!')
   end
 end
